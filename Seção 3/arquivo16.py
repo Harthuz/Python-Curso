9f0c2006c98d56690de1e2299bc686e7d1375c98
Harthuz <hernandeshass910@gmail.com>
2023-10-02 21:46:17 -0300

    Is, Is not e None

---
+++ Seção 3/arquivo16.py
@@ -0,0 +1,14 @@
+PASSOU = None
+condicao = False
+
+if condicao is True:
+    PASSOU = True
+    print(f'Condição é {condicao}')
+else:
+    print(f'Condição é {condicao}')
+
+if PASSOU is True:
+    print(f'Passou: {PASSOU}')
+
+if PASSOU is not True:
+    print(f'Passou: {PASSOU}')
