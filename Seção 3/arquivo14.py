e996818ade925a046b6d9ac8b328d3db286e88de
Harthuz <hernandeshass910@gmail.com>
2023-10-02 21:02:58 -0300

    Radar carro - Corrigido

--- Seção 3/arquivo14.py
+++ Seção 3/arquivo14.py
@@ -1,11 +1,12 @@
-velocidade = 100
-local = 98 #mudar o local para passar pelo radar
+velocidade = 60 #mudar a velocidade
+local = 100 #mudar o local para passar pelo radar
 
 RADAR_1 = 60
 LOCAL_1 = 100
 RADAR_RANGE = 1
 
-if velocidade > RADAR_1 and (local == LOCAL_1 - RADAR_RANGE) or (local == LOCAL_1 + RADAR_RANGE) or (local == LOCAL_1):
-    print('Você foi multado')
-else:
-    print('Você não foi multado')
+if velocidade > RADAR_1:
+    print('Você passou pelo radar 1')
+
+if local >= (LOCAL_1 - RADAR_RANGE) and local <= (LOCAL_1 + RADAR_RANGE) and velocidade > RADAR_1:
+    print('Você foi multado no radar 1')
