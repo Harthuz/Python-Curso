b9702ab018e3404f0efcd7f6b2ad056b6e930e6e
Harthuz <hernandeshass910@gmail.com>
2023-10-04 19:32:02 -0300

    Aula 55 - Correção do exercicio 1

--- Seção 3/exercicio5.1.py
+++ Seção 3/exercicio5.1.py
@@ -1,6 +1,9 @@
 numero_inteiro = input('Digite um número inteiro: ')
 
-if int(numero_inteiro)%2==0:
-    print('O número digitado é par')
-else:
-    print('O número digitado é impar')
+try:
+    if int(numero_inteiro)%2==0:
+        print('O número digitado é par')
+    else:
+        print('O número digitado é impar')
+except:
+    print('Você não digitou um número inteiro.')
