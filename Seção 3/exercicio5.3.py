e7070c9390a37658746a63bfbcf9451fdf6371c5
Harthuz <hernandeshass910@gmail.com>
2023-10-04 19:40:08 -0300

    Aula 56 - Coreção do exercício 2

--- Seção 3/exercicio5.3.py
+++ Seção 3/exercicio5.3.py
@@ -1,11 +1,12 @@
 primeiro_nome = input('Digite seu primeiro nome: ')
 tamanho_nome = len(primeiro_nome)
 
-if tamanho_nome == 0:
+if primeiro_nome:
+    if tamanho_nome>0 and tamanho_nome<=4:
+        print('Seu nome é curto.')
+    elif tamanho_nome>4 and tamanho_nome<7:
+        print('Seu nome é normal.')
+    elif tamanho_nome>6:
+        print('Seu nome é muito grande.')
+else:
     print('Você não digitou nada.')
-elif tamanho_nome>0 and tamanho_nome<=4:
-    print('Seu nome é curto.')
-elif tamanho_nome>4 and tamanho_nome<7:
-    print('Seu nome é normal.')
-elif tamanho_nome>6:
-    print('Seu nome é muito grande.')
