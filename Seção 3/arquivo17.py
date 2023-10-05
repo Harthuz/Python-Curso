eaba165de2a8247fb0ddb1bfdd729b9a3f1a0c6f
Harthuz <hernandeshass910@gmail.com>
2023-10-04 22:03:27 -0300

    Aula 59 a 63 - while, continue, break...

---
+++ Seção 3/arquivo17.py
@@ -0,0 +1,27 @@
+# porteira = True
+
+# while porteira:
+#     nome = input('Digite o seu nome: ')
+#     print(f'O seu nome é {nome}')
+#     if nome == 'Hernandes':
+#         break
+
+# print('While acabou')
+
+# contador = 0
+
+# while contador<10:
+#     contador+=1
+
+#     if contador == 5:
+#         continue
+        
+#     print(contador)
+
+contador = 0 
+while contador<100:
+    contador+=1
+    while contador<50:
+        print('Não alcansamos 50')
+        contador+=1
+    print(contador)
