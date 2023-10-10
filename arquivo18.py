076ff5f7b0d2422442164e8d8abc8720a33ee42a
Harthuz <hernandeshass910@gmail.com>
2023-10-09 21:44:39 -0300

    Aula 72 a 74 - range e for

---
+++ arquivo18.py
@@ -0,0 +1,21 @@
+# ---Aula 72---
+
+# frase = 'Pititaquiri'
+# i = 0
+
+# for letra in frase:
+#     print(letra.upper())
+
+# ---Aula 73---
+
+# numeros = range(0, 11 , 2)
+
+# for numero in numeros:
+#     print(numero)
+
+nome = iter('Hernandes')
+print(nome)
+
+print(next(nome))
+print(next(nome))
+print(next(nome))
