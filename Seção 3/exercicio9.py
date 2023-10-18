cc95308eca1120788acd22a7bdf4f8e40fd19d00
Harthuz <hernandeshass910@gmail.com>
2023-10-17 22:00:53 -0300

    ...

--- Seção 3/exercicio9.py
+++ Seção 3/exercicio9.py
@@ -1,72 +1,30 @@
-print('\n Olá :) \n Seja bem vindo ao Adivinhord \n Para jogar apenas digite uma letra, e eu te direi se a letra digitada está na palavra que eu estou pensando... \n Vamos começar! \n')
+palavra_secreta = 'hernandes'
+letras_acertadas = ''
+numero_tentativas = 0
 
-palavra_secreta = "hernandes"
-tamanho_palavra_secreta = len(palavra_secreta)
-acertou = False
-quantidade_letras_certas = 0
-letras_certas = ""
-tentativas = 0
+while True:
+    letra_digitada = input('Digite uma letra: ')
+    numero_tentativas += 1
 
-while acertou != True:
-    status_palavra = ""
-
-
-    if quantidade_letras_certas < 1:
-        chute = input('Digite a primeira letra: ')
-    else:
-        chute = input('Digite outra letra: ')
-
-    if len(chute) > 1: # verificando se o usuário digitou mais que uma letra
-        print('\n Digite apenas uma letra.\n')
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra.')
         continue
 
-    if chute in palavra_secreta:
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
 
-        letras_certas += chute
-
-        if quantidade_letras_certas == 0:
-            print('\n Você acertou uma letra \n')
-        if quantidade_letras_certas > 0:
-            print('\n Você acertou outra letra \n')
-        
-        quantidade_letras_certas += 1
-
-        if "h" and "e" and "r" and "n" and "a" and "n" and "d" and "e" and "s" in letras_certas:
-            acertou = True
-
-        
-        
-    else:
-        if quantidade_letras_certas < 1:
-            print('\n Vish \n Parece que você começou com o pé direito \n Tente novamente \n')
-        elif quantidade_letras_certas == 1:
-            print('\n Tente novamente \n')
-        elif quantidade_letras_certas >1 and quantidade_letras_certas < 3:
-            print('\n Tente novamente \n')
-        elif quantidade_letras_certas >2 and quantidade_letras_certas < 5:
-            print('\n Continue a nadar... continue a nadar... \n')
-        elif quantidade_letras_certas == 5:
-            print('\n Continue tentando, uma hora você consegue \n Ou não :/ \n')
-        elif quantidade_letras_certas == 6:
-            print('\n Parece que eu peguei pesado na palavra escolhida :| \n Mas não desista \n')
-        elif quantidade_letras_certas == 20:
-            print('\n Eu vou dormir, não aguento mais esperar \n')
-        elif quantidade_letras_certas >20:
-            print('\n Zzzzzzzzzzzzzzzzzz \n')
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
         else:
-            print('\n Tente novamente \n')
+            palavra_formada += '*'
 
+    print('Palavra formada:', palavra_formada)
 
-    for chute in letras_certas:
-        if chute in letras_certas:
-            print(chute)
-        else:
-            print('*')
-    
-    print(f'{status_palavra} \n')
-
-    tentativas += 1
-
-print(f'\n Parabéns, você venceu \n A palavra era "{palavra_secreta}" \n você precisou de {tentativas} tentativas para acertar \n')
-
-# não consegui imprimir a "status_palavra" do jeito que deveria e não criei uma verificação caso o usuário digite mais de uma letra
+    if palavra_formada == palavra_secreta:
+        print('VOCÊ GANHOU!! PARABÉNS!')
+        print('A palavra era', palavra_secreta)
+        print('Tentativas:', numero_tentativas)
+        letras_acertadas = ''
+        numero_tentativas = 0
