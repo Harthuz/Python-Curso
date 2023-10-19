1107cb303a6f4417cb2e8828b6d733fbdbdf35a4
Harthuz <hernandeshass910@gmail.com>
2023-10-19 09:08:08 -0300

    Aula 80 e 81 - Lista (list)

--- Seção 3/arquivo20.py
+++ Seção 3/arquivo20.py
@@ -7,6 +7,26 @@
 # lista = [123, 'Hernandes', False, 3.14]
 # print(lista[1].upper())
 
+# lista = [123, 'Hernandes', False, 3.14]
+# lista[1] = 'Bolinho'
+# print(lista[1])
+
+# lista = [123, 'Hernandes', False, 3.14]
+# del lista[1]
+# print(lista)
+
+# lista = [123, 'Hernandes', False, 3.14]
+# lista.append('adição')
+# print(lista)
+
+# lista = [123, 'Hernandes', False, 3.14]
+# lista.pop()
+# print(lista)
+
+# lista = [123, 'Hernandes', False, 3.14]
+# valor_removido = lista.pop()
+# print(lista, 'valor removido: ', valor_removido)
+
 lista = [123, 'Hernandes', False, 3.14]
-lista[1] = 'Bolinho'
-print(lista[1])
+lista.pop(0)
+print(lista)
