5fd665535037065c5bc9c8419899726d1ae11f9d
Harthuz <hernandeshass910@gmail.com>
2023-10-19 08:29:16 -0300

    Aula 80 - Lista (list)

---
+++ Seção 3/arquivo20.py
@@ -0,0 +1,12 @@
+# lista = bool([])
+# print(lista)
+
+# lista = [123, 'Hernandes', False, 3.14]
+# print(lista)
+
+# lista = [123, 'Hernandes', False, 3.14]
+# print(lista[1].upper())
+
+lista = [123, 'Hernandes', False, 3.14]
+lista[1] = 'Bolinho'
+print(lista[1])
